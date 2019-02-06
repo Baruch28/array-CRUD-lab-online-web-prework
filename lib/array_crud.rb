8710f2926c2ca9f809bc95857553d49bf9fb4e82
mark baruch <mbaruch28@gmail.com>
2019-02-06 15:46:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -11,15 +11,15 @@ array.push(vegetable)
 end
 
 def add_element_to_start_of_array(array, element)
-
+array.unshift(vegetable)
 end
 
 def remove_element_from_end_of_array(array)
-
+array.pop(grain)
 end
 
 def remove_element_from_start_of_array(array)
-
+array.shift(vegetable)
 end
 
 def retrieve_element_from_index(array, index_number)
